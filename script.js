427891b59d90fbc942b6ea0eee35eac167bce13e
illicitgarcon <163777356+illicitgarcon@users.noreply.github.com>
2026-09-17 18:56:42 +0000

    my 2

---
+++ script.js
@@ -0,0 +1,19 @@
+function openMenu(){
+
+document
+.getElementById("menu")
+.classList
+.add("active");
+
+}
+
+
+
+function closeMenu(){
+
+document
+.getElementById("menu")
+.classList
+.remove("active");
+
+}
